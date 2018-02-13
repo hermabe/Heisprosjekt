d4e64102aac0a21f1347f858f8e57ebde5b47a4d
hermabe <32880010+hermabe@users.noreply.github.com>
2018-02-13 11:22:59 +0100

    Implemented initialize()

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "elev.h"
 #include <stdio.h>
 
+void initialize(){
+    printf("Initializing elevator\n");
+    elev_set_motor_direction(DIRN_DOWN);
+    while (elev_get_floor_sensor_signal() != (int)0) {}
+    elev_set_motor_direction(DIRN_UP);
+    elev_set_motor_direction(DIRN_STOP);
+}
+
 
 int main() {
     // Initialize hardware
@@ -11,22 +19,23 @@ int main() {
 
     printf("Press STOP button to stop elevator and exit program.\n");
 
-    elev_set_motor_direction(DIRN_UP);
+    initialize();
+    printf("Idle");
+    
+    // while (1) {
+    //     // Change direction when we reach top/bottom floor
+    //     if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
+    //         elev_set_motor_direction(DIRN_DOWN);
+    //     } else if (elev_get_floor_sensor_signal() == 0) {
+    //         elev_set_motor_direction(DIRN_UP);
+    //     }
 
-    while (1) {
-        // Change direction when we reach top/bottom floor
-        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
-            elev_set_motor_direction(DIRN_DOWN);
-        } else if (elev_get_floor_sensor_signal() == 0) {
-            elev_set_motor_direction(DIRN_UP);
-        }
-
-        // Stop elevator and exit program if the stop button is pressed
-        if (elev_get_stop_signal()) {
-            elev_set_motor_direction(DIRN_STOP);
-            break;
-        }
-    }
+    //     // Stop elevator and exit program if the stop button is pressed
+    //     if (elev_get_stop_signal()) {
+    //         elev_set_motor_direction(DIRN_STOP);
+    //         break;
+    //     }
+    // }
 
     return 0;
 }

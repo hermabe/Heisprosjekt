d5df83c0437568d3668c43f2b70d1f34edb64b2d
mahoyen <32592450+mahoyen@users.noreply.github.com>
2018-02-13 12:47:00 +0100

    Turns lamp on when buttonpress

--- buttonRead.c
+++ buttonRead.c
@@ -8,8 +8,11 @@
 int read_buttons_and_light_up_button(int elevator_location) {
     for (int floor=0; floor<N_FLOORS;floor++) {
         for (elev_button_type_t button = BUTTON_CALL_UP; button < 3; button++) {
-            if (elev_get_button_signal(button, floor)==1)
+            if (elev_get_button_signal(button, floor)==1) {
+                elev_set_button_lamp(button, floor, 1);
+            }
 
         }
     }
 }
+

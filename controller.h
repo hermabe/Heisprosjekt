1f5dfdd738238d6be4477352b4be120a447de351
hermabe <32880010+hermabe@users.noreply.github.com>
2018-02-20 11:19:05 +0100

    Defined states_t

--- controller.h
+++ controller.h
@@ -4,19 +4,23 @@
 #include <stdbool.h>
 
 /**
+ * Defines all possible states
+ * */
+typedef enum tag_states {INIT, IDLE, STOP, UP, DOWN, UPWAIT, DOWNWAIT} States_t;
+
+/**
  * Holds all the values for queue-system
  **/
-
 typedef struct tag_controller{
     unsigned int current_floor;
     unsigned int state;
     bool up_queue[4];
     bool down_queue[4];
-} Controller;
+} Controller_t;
 
 
-bool is_queue_empty(elev_motor_direction_t dir, const Controller ctrl);
-elev_motor_direction_t up_or_down_from_idle(const Controller ctrl);
+bool is_queue_empty(elev_motor_direction_t dir, const Controller_t ctrl);
+elev_motor_direction_t up_or_down_from_idle(const Controller_t ctrl);
 
 /**
  * Initializes the hardware and puts the elevator in a known position (1st floor)

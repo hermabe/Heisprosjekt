de3659c9ae226c739c6a5191b2049b58644ac6f8
mahoyen <32592450+mahoyen@users.noreply.github.com>
2018-02-20 11:18:51 +0100

    Added remove_floor and reached_a_floor
    remove_floor is finished

--- controller.c
+++ controller.c
@@ -25,3 +25,91 @@ void startup()
     elev_set_motor_direction(DIRN_STOP);
     printf("Initialization done\n");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void remove_floor(elev_motor_direction_t direction, controller_t ctrl, floor) {
+    assert(direction != DIRN_STOP);
+    if (direction == DIRN_UP) {
+        up_queue[floor] = 0;
+    } else if (direction == DIRN_UP) {
+        down_queue[floor] = 0;
+    }
+}
+
+void reached_a_floor(elev_motor_direction_t direction, controller_t ctrl) {
+    int floor=(elev_get_floor_sensor_signal();
+    if (floor!=-1) {
+        if (direction == DIRN_UP) || (direction == DIRN_DOWN) {
+            remove_floor();
+
+        }
+    }
+}
--- controller.h
+++ controller.h
@@ -7,3 +7,18 @@
  * Initializes the hardware and puts the elevator in a known position (1st floor)
  */
 void startup();
+
+
+
+/****
+ * Removes floor in the right direction
+ * 
+ */
+
+void remove_floor(elev_motor_direction_t direction, controller_t ctrl, floor)
+
+/*
+* Check if one have reached a floor and checks queues and performes action
+*/
+
+void reached_a_floor(elev_motor_direction_t direction, controller_t ctrl)   

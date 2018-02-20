0cd68d0e62048bd9aeee9fa7c023f0d51be7a4c3
mahoyen <32592450+mahoyen@users.noreply.github.com>
2018-02-20 14:17:39 +0100

    added elev_get_button_lamp in headerfile

--- elev.h
+++ elev.h
@@ -118,6 +118,6 @@ int elev_get_button_signal(elev_button_type_t button, int floor);
 */
 void elev_set_button_lamp(elev_button_type_t button, int floor, int value);
 
-
+int elev_get_button_lamp(elev_button_type_t button, int floor);
 
 #endif // #ifndef __INCLUDE_DRIVER_H__

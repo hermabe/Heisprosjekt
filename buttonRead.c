85313133b771a6513c8d8610f6984298180b00fb
mahoyen <32592450+mahoyen@users.noreply.github.com>
2018-02-27 13:08:41 +0100

    changed function type

--- buttonRead.c
+++ buttonRead.c
@@ -5,7 +5,7 @@
  * 
  * 
  * ****************************************************/
-int read_buttons_and_light_up_button() {
+void read_buttons_and_light_up_button() {
     for (int floor=0; floor<N_FLOORS;floor++) {
         for (elev_button_type_t button = BUTTON_CALL_UP; button < 3; button++) {
             if (elev_get_button_signal(button, floor)==1) {

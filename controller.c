ad5cb06c35b766d7f4da8dbb26650724ff9f2a2a
hermabe <32880010+hermabe@users.noreply.github.com>
2018-02-27 11:48:52 +0100

    Fixed syntax errors

--- controller.c
+++ controller.c
@@ -65,48 +65,41 @@ void reset_lights(int floor) {
 }
 
 void add_floors(Controller_t *ctrl) {
-    elev_motor_direction_t direction = crtl->direction;
+    elev_motor_ctrl->direction_t ctrl->direction = ctrl->direction;
     for (unsigned int floor = 0; floor++; floor < 4) {
-        for(elev_button_type_t button=BUTTON_CALL_UP; button++; button <= BUTTON_COMMAND) {
-            
-            
-            button_pressed = elev_get_button_lamp(button, floor);
-            if button_pressed {
-                add_button_to_queue(ctrl, button, floor)
+        for(elev_button_type_t button=BUTTON_CALL_UP; button++; button <= BUTTON_COMMAND) {            
+            if (elev_get_button_lamp(button, floor)) {
+                add_button_to_queue(ctrl, button, floor);
             }
         }
     }
 }
 
 void add_button_to_queue(Controller_t *ctrl, elev_button_type_t button, unsigned int floor) {
-    bool above_elevator
-    if (floor<ctrl->current_floor) {
-        above_elevator = true;
-    } else {
-        above_elevator = false;
-    }
+    bool above_elevator = (floor < ctrl->current_floor);
+
     switch (button) {
         case BUTTON_CALL_UP: {
-            if above_elevator {
-                if (direction >= 0) {
+            if (above_elevator) {
+                if (ctrl->direction >= 0) {
                     ctrl->queues[0][floor] = true; //Add in primary queue
                 } else {
                     ctrl->queues[2][floor] = true; //Add in extra queue
                 }
             } else {
-                if (direction > 0) {
+                if (ctrl->direction > 0) {
                     ctrl->queues[1][floor] = true; //Add in secondary queue
                 }
             }
         }
         case BUTTON_CALL_DOWN: {
-            if above_elevator {
-                if (direction > 0) {
+            if (above_elevator) {
+                if (ctrl->direction > 0) {
                     ctrl->queues[1][floor] = true; 
                 }
             } else {
                 
-                if (direction <= 0) {
+                if (ctrl->direction <= 0) {
                     ctrl->queues[0][floor] = true; 
                 } else { 
                     ctrl->queues[2][floor] = true; 
@@ -114,14 +107,14 @@ void add_button_to_queue(Controller_t *ctrl, elev_button_type_t button, unsigned
             }
         }
         case BUTTON_COMMAND: {
-            if above_elevator {
-                if (direction <= 0) {
+            if (above_elevator) {
+                if (ctrl->direction <= 0) {
                     ctrl->queues[0][floor] = true;
                 } else {
                     ctrl->queues[2][floor] = true;
                 }
             } else {
-                if (direction > 0) {
+                if (ctrl->direction > 0) {
                     ctrl->queues[1][floor] = true;
                 }
             }
@@ -159,7 +152,7 @@ bool is_queue_empty(const bool queue[], const int size)
     return true;
 }
 
-// bool is_queue_empty(elev_motor_direction_t dir, const Controller_t ctrl)
+// bool is_queue_empty(elev_motor_ctrl->direction_t dir, const Controller_t ctrl)
 // {
 //     if (dir == DIRN_DOWN)
 //     {
@@ -213,7 +206,7 @@ void check_stop(Controller_t* ctrl){
 void initialize_controlstruct(Controller_t *ctrl, unsigned int current_floor, State_t state) {
     ctrl->current_floor = current_floor;
     ctrl->state = state;
-    ctrl->direction = DIRN_STOP;
+    ctrl->ctrl->direction = DIRN_STOP;
 
     for (int i = 0; i < 3; ++i){
         for (int j = 0; j < 4; ++j){

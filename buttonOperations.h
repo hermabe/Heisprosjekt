d16654e972276057558656c4bc0ad543f409918f
hermabe <32880010+hermabe@users.noreply.github.com>
2018-03-11 20:56:13 +0100

    Comments in buttonOperations.h

--- buttonOperations.h
+++ buttonOperations.h
@@ -5,17 +5,17 @@
 #include "controller.h"
 
 /**
- * Read buttons and add to queue
+ * Read buttons and turn on lights if button pressed
  */
 void read_buttons_and_light_up_buttons();
 
 /**
- * Resets buttons at a floor
+ * Resets buttons at a floor, ie. turns off the lights
  * @param floor the floor to reset all buttons
 */
 void reset_button_lights_at_floor(int floor);
 
 /**
- * Resets all light except lamp
+ * Resets all light except stop light
 */
 void reset_button_lights_except_stop_light();
